421805b45e80f93a130abd225c30cee1cb76f0bd
Nikhil <guptanikhil801@gmail.com>
2023-12-20 18:14:20 +0530

    required packages added, entity framework setup done

---
+++ TaskManagement/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticeWebApi.Contexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TaskManagement.Models.Task> Tasks { get; set; }
+    }
+}
---
+++ TaskManagement/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagement.Models
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Enums.TaskStatus Status { get; set; }
+    }
+}
